49154a1dcf90b6ccf79fd69457f788d271848adc
Jeanette Conklin <conklinj09@gmail.com>
2025-03-20 21:34:59 -0700

    Added Contact.jsx

---
+++ public/src/components/Contact.jsx
@@ -0,0 +1,15 @@
+// src/components/Contact.jsx
+import React from "react";
+import "../styles/contact.css";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <h2>Contact</h2>
+      <p>If you have questions or feedback, hop on over and send a message!</p>
+      <p>Email: contact@wellnesslogfrog.com</p>
+    </div>
+  );
+}
+
+export default Contact;
