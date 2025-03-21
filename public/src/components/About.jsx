25403022169433c81c60ec389ab8bc6b5f49101c
Jeanette Conklin <conklinj09@gmail.com>
2025-03-20 21:32:14 -0700

    Added About.jsx

---
+++ public/src/components/About.jsx
@@ -0,0 +1,18 @@
+// src/components/About.jsx
+import React from "react";
+import "../styles/about.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      <h2>About Wellness Log Frog</h2>
+      <p>
+        This app was created to support mental wellness through consistent,
+        intentional habits. It’s gentle, frog-themed, and designed with love for those
+        who are healing, growing, and glowing. 🐸💚
+      </p>
+    </div>
+  );
+}
+
+export default About;
